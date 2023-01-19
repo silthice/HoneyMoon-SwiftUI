b603f4238a42e4a0b6ee2ea78c3ef5eab0985e03
silthice <ryonlim@hotmail.com>
2023-01-19 15:25:49 +0800

    -Test commit

---
+++ Honeymoon/Utilities/ETSt.swift
@@ -0,0 +1,20 @@
+//
+//  ETSt.swift
+//  Honeymoon
+//
+//  Created by Giap on 19/01/2023.
+//
+
+import SwiftUI
+
+struct ETSt: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ETSt_Previews: PreviewProvider {
+    static var previews: some View {
+        ETSt()
+    }
+}
