6c1826c1233a74298ee0b6b105157a5a27f23ef5
silthice <ryonlim@hotmail.com>
2023-01-19 18:20:21 +0800

    - Added swiping animation and card render logic

--- Honeymoon/Apps/ContentView.swift
+++ Honeymoon/Apps/ContentView.swift
@@ -12,24 +12,103 @@ struct ContentView: View {
     @State var showAlert: Bool = false
     @State var showGuide: Bool = false
     @State var showInfo: Bool = false
+    @GestureState private var dragState = DragState.inactive
+    
+    //MARK: - CARD VIEWS
+    var cardViews: [CardView] = {
+        var views = [CardView]()
+        //Optimize to 2 cards only instead whole array of card views
+        for index in 0..<2 {
+            views.append(CardView(honeyMoon: honeymoonData[index]))
+        }
+        return views
+    }()
+    
+    //MARK: - TOP CARD
+    private func isTopCard(cardView: CardView) -> Bool {
+        guard let index = cardViews.firstIndex(where: {$0.id == cardView.id}) else {
+            return false
+        }
+        return index == 0
+    }
+    
+    
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self {
+            case .inactive, .pressing :
+                return .zero
+            case .dragging(let translation):
+                return translation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self {
+            case .dragging:
+                return true
+            case .pressing, .inactive:
+                return false
+            }
+        }
+        
+        var isPressing: Bool {
+            switch self {
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
+    }
     
     //MARK: - BODY
     var body: some View {
         
         VStack {
-            
             //MARK: - HEADER
             HeaderView(showGuideView: $showGuide, showInfoView: $showInfo)
+                .opacity(dragState.isDragging ? 0 : 1)
+                .animation(.default, value: dragState.isDragging)
             Spacer()
             
-            //MARK: - CARDVIEW
-            CardView(honeyMoon: honeymoonData[2])
-                .padding()
+            //MARK: - CARDS
+            ZStack {
+                ForEach(cardViews) { cardView in
+                    cardView
+                        .zIndex(isTopCard(cardView: cardView) ? 1 : 0)
+                        .offset(x: isTopCard(cardView: cardView) ? dragState.translation.width : 0, y: isTopCard(cardView: cardView) ? dragState.translation.height : 0)
+                        .scaleEffect(dragState.isDragging && isTopCard(cardView: cardView) ? 0.85 : 1)
+                        .rotationEffect(Angle(degrees: isTopCard(cardView: cardView) ? Double(dragState.translation.width / 12 ) : 0 ))
+                        .animation(.interpolatingSpring(stiffness: 120, damping: 120), value: dragState.isDragging)
+                        .gesture(
+                            LongPressGesture(minimumDuration: 0.01)
+                                .sequenced(before: DragGesture())
+                                .updating(self.$dragState, body: {(value, state, transaction) in
+                                    switch value {
+                                    case .first(true) :
+                                        state = .pressing
+                                    case .second(true, let drag):
+                                        state = .dragging(translation: drag?.translation ?? .zero)
+                                    default:
+                                        break
+                                    }
+                                })
+                        )
+                }
+            }
+            .padding(.horizontal)
             
             Spacer()
             
             //MARK: - FOOTER
             FooterView(showBookingAlert: $showAlert)
+                .opacity(dragState.isDragging ? 0 : 1)
+                .animation(.default, value: dragState.isDragging)
         } //: End of VStack
         .alert(isPresented: $showAlert, content: {
             Alert(
--- Honeymoon/Views/CardView.swift
+++ Honeymoon/Views/CardView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct CardView: View {
+struct CardView: View, Identifiable {
     //MARK: - PROPERTIES
     var honeyMoon: Destination
     let id = UUID()
