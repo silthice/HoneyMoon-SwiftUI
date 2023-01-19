91af26db8b6ba34115163a321cae7ac6528fbe36
silthice <ryonlim@hotmail.com>
2023-01-19 15:22:54 +0800

    - Initial setups

---
+++ Honeymoon/Apps/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Giap on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
